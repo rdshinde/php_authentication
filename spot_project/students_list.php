64aef25279cd1e9e219f8cc6f3a32a05c1432ff9
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2021-11-19 16:50:51 +0530

    admin section done

--- spot_project/students_list.php
+++ spot_project/students_list.php
@@ -1,39 +1,70 @@
 <?php 
   include_once 'base.php';
-  include_once 'spot_project/includes/dbh.inc.php';
+  include_once 'includes/dbh.inc.php';
 ?>
-    <main class="container d-flex justify-content-center align-items-center">
+    <main class="container d-flex justify-content-center align-items-center flex-column">
       <div class="row display-3 m-5 p-lg-5 shadow rounded h-100 text-info ">
 
       
       <h1 class="d-flex justify-content-center align-items-center flex-column ">
       <?php
               if(isset($_SESSION["userid"])){
-                echo 'Welcome'."  "."<b style='color:red;'>".$_SESSION["name"]."</b>".$_SESSION["isAdmin"] ;
+                echo 'Welcome Admin'."  "."<b style='color:red;'>".$_SESSION["name"]."</b>" ;
               }
               else{
                 echo 'Welcome To Authentication Homepage';
               }
+
+              
             ?>
        
       </h1>
       </div>
               <div>
-              <?php
-                  $sql = "SELECT * FROM users";
+              <div class="table-responsive">
+          <table class="table table-hover p-3">
+            <thead>
+              <tr>
+                <th scope="col">Sr.No.</th>
+                <th scope="col">Firstname</th>
+                <th scope="col">Lastname</th>
+                <th scope="col">Branch</th>
+                <th scope="col">Email</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+                <?php 
+                
+                $sql = "SELECT * FROM users";
                   $results = mysqli_query($conn, $sql);
                   $resultCheck = mysqli_num_rows($results);
 
                   if($resultCheck > 0){
-                    
+                    $sr = 1;
                     while($row = mysqli_fetch_assoc($results)){
-                      echo $row;
+                      $firstname = $row["usersFirstname"];
+                      $lastname = $row["usersLastname"];
+                      $branch = $row["usersBranch"];
+                      $email = $row["usersEmail"];
+                      echo '<tr>
+                      <th scope="row">'.$sr.'</th>
+                      <td>'.$firstname.'</td>
+                      <td>'.$lastname.'</td>
+                      <td>'.$branch.'</td>
+                      <td>'.$email.'</td>
+                          </tr>';
+                        $sr = $sr+1;
+                      
                     }
                   }
                   else{
                     echo '<h1 class="d-flex justify-content-center align-items-center flex-column "> No Data Found </h1>';
                   }
-              ?>
+                ?>
+                </tbody>
+          </table>
+      </div>
               </div>
     </main>
   </body>

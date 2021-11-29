abf4edfe8aa0751dab68c42f45cde3db99f14987
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2021-11-29 13:03:02 +0530

    message receiving part done

---
+++ spot_project/recieved_msg.php
@@ -0,0 +1,58 @@
+<?php 
+  include_once 'base.php';
+  include_once 'includes/dbh.inc.php';
+?>
+
+        <div class="container px-lg-2 my-5">
+            <!-- alerts -->
+        <div class="p-3 p-lg-2 bg-light rounded-3">
+            <h1 class="display-5 text-center mb-5">Messages Received</h1>
+            <div class="m-1 m-lg-1">
+                <section>
+                <div
+                    class="
+                    container
+                    p-3
+                    col-lg-6 col-xxl-12 col-centered
+                    text-center
+                    d-flex
+                    flex-wrap
+                    justify-content-evenly
+                    ">
+                    <?php
+                    $srollno = $_SESSION["rollno"];
+                    $sql = "SELECT * FROM messages WHERE rollno=$srollno";
+                    $results = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($results);
+                    if($resultCheck > 0){
+                      while($row = mysqli_fetch_assoc($results)){
+                        $name = $row["mfrom"];
+                        $id = $row["msgId"];
+                        $branch = $row["branch"];
+                        $rollno = $row["rollno"];
+                        $subject = $row["topic"];
+                        $msg = $row["msg"];
+                        $timestamp = $row["mtimestamp"];
+                        echo '<article>
+                        <div class="card mb-3" style="width: 25rem;">
+                            <div class="card-body">
+                                <h5 class="card-title h5"> From: '.$name.'</h5>
+                                <p class="card-subtitle mb-2 h5"><strong> Response to: '.$subject.'</strong></p><span><small> Time: '.$timestamp.'</small></span>
+                                <p class="card-text lead">'.$msg.'</p>
+                            </div>
+                        </div>
+                        </article>';
+                                    }
+                                }
+                                else{
+                                echo '<h2 class="d-flex justify-content-center align-items-center flex-column dispaly-6"> No Data Found </h2>';
+                                }
+                            ?>
+                        
+                   </div>
+                </section>
+            </div>
+        </div>
+    </div>    
+    </body>
+</html>
